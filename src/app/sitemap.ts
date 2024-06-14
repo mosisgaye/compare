0d79e8aef96a19a22a24f6310ed357fcde3ce4d2
Moussa <mosisg7@gmail.com>
2024-06-14 19:39:11 +0100

    Mise à jour des fichiers et ajout de nouvelles images et composants

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,39 +1,30 @@
-import { type MetadataRoute } from 'next';
-import { categories } from '@/config';
-import prisma from '@/lib/db';
+import { type MetadataRoute } from 'next'
+
+import { categories } from '@/config'
+import prisma from '@/lib/db'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
-  if (!baseUrl) {
-    throw new Error('NEXT_PUBLIC_APP_URL is not defined in your environment variables');
-  }
-
-  const staticRoutes = ['', 'about', 'contact', 'terms', 'privacy'].map((route) => ({
-    url: `${baseUrl}/${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-
   const allProducts = await prisma.product.findMany({
     select: {
-      id: true,
+      id: true,  
       slug: true,
     },
-  });
+  })
 
-  const productsRoutes = allProducts.map((product) => {
-    const url = new URL(`${baseUrl}/${product.id}/${product.slug}`);
-    url.searchParams.set('productId', product.id.toString());
-
-    return {
-      url: url.toString(),
-      lastModified: new Date().toISOString(),
-    };
-  });
+  const productsRoutes = allProducts.map((product) => ({
+    url: `${process.env.NEXT_PUBLIC_APP_URL}/${product.id}/${product.slug}?productId=${product.id}`,
+    lastModified: new Date().toISOString(),
+  }))
 
   const categoriesRoutes = categories.map((category) => ({
-    url: `${baseUrl}/products?category=${category.slug}`,
+    url: `${process.env.NEXT_PUBLIC_APP_URL}/products?category=${category.slug}`,
     lastModified: new Date().toISOString(),
-  }));
+  }))
 
-  return [...staticRoutes, ...productsRoutes, ...categoriesRoutes];
+  const routes = ['', 'products'].map((route) => ({
+    url: `${process.env.NEXT_PUBLIC_APP_URL}/${route}`,
+    lastModified: new Date().toISOString(),
+  }))
+
+  return [...routes, ...productsRoutes, ...categoriesRoutes]
 }
